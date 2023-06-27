39342426f13cd6b85b92fe93e6fa919c956b3733
MoonPrograms <112990268+Sn4keProgr4ms@users.noreply.github.com>
2023-06-27 10:44:25 -0500

    Update Lab1.ino

--- src/Lab1.ino
+++ src/Lab1.ino
@@ -8,10 +8,15 @@
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
+pinMode(D5, OUTPUT);
 
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
+  digitalWrite(D5, HIGH);
+delay(500);
+  digitalWrite(D5, LOW);
+delay(500);
   // The core of your code will likely live here.
 
